d50218c67492d310febe892e506976e15b135fca
kswoong1819 <60123109+kswoong1819@users.noreply.github.com>
2021-02-26 17:28:54 +0900

    Create BOJ_17831.c

---
+++ Project12/Project12/BOJ_17831.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define MaxSz 200001
+
+int n;
+int value[MaxSz];
+int visited[MaxSz];
+
+void fullvector(int num, int** arr) {
+	arr[num][0] += 100;
+	realloc(arr[num], sizeof(int*) * arr[num][0]);
+	return;
+}
+
+int max(int* dp) {
+	int x = 0, maxVal = 0;
+	while (dp[x] > 0) {
+		if (maxVal < dp[x])
+			maxVal = dp[x];
+	}
+	return maxVal;
+}
+
+void dfs(int** arr, int** dp, int k) {
+	visited[k] = 1;
+	for (int i = 0; i < arr[k][1]; i++) {
+		int nxt = arr[k][i+2];
+		if (visited[nxt] == 1) continue;
+		dfs(arr, dp, nxt);
+		dp[k][i] += value[k] * value[nxt];
+		int sum = max(dp[nxt]);
+		for (int j = 0; j <= arr[k][1]; j++) {
+			if (i == j) continue;
+			dp[k][j] += sum;
+		}
+	}
+	return;
+}
+
+int main() {
+	int num;
+	freopen("input.txt", "r", stdin);
+	scanf("%d", &n);
+	int** arr = (int**)malloc(sizeof(int*) * (n + 1));
+	for (int i = 1; i <= n; i++) {
+		arr[i] = (int*)malloc(sizeof(int) * 100);
+		arr[i][0] = 100;
+		arr[i][1] = 2;
+	}
+	for (int i = 2; i <= n; i++) {
+		scanf("%d", &num);
+		arr[num][arr[num][1]++] = i;
+		if (arr[num][1] >= arr[num][0])
+			fullvector(num, arr);
+	}
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &value[i]);
+	}
+	int** dp = (int**)malloc(sizeof(int*) * (n + 1));
+	for (int i = 1; i <= n; i++) {
+		dp[i] = (int*)malloc(sizeof(int) * arr[i][0]);
+	}
+
+	dfs(arr, dp, 1);
+	printf("%d\n", max(dp[1]));
+
+	for (int i = 1; i <= n; i++) {
+		free(arr[i]);
+		free(dp[i]);
+	}
+	free(arr);
+	free(dp);
+	return 0;
+}
