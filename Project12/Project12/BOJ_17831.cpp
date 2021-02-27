12dec1e85164ae14638a15530d33ac2f9cf4d145
kswoong1819 <kswoong1819@gmail.com>
2021-02-28 01:27:47 +0900

    0227

--- Project12/Project12/BOJ_17831.cpp
+++ Project12/Project12/BOJ_17831.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 
-#define MaxSz 200001
+#define MaxSz 200010
 
 int n;
 int value[MaxSz];
@@ -14,24 +14,28 @@ void fullvector(int num, int** arr) {
 }
 
 int max(int* dp) {
-	int x = 0, maxVal = 0;
+	int x = 1, maxVal = 0;
 	while (dp[x] > 0) {
 		if (maxVal < dp[x])
 			maxVal = dp[x];
+		x++;
 	}
 	return maxVal;
 }
 
 void dfs(int** arr, int** dp, int k) {
 	visited[k] = 1;
-	for (int i = 0; i < arr[k][1]; i++) {
-		int nxt = arr[k][i+2];
+	for (int i = 1; i < arr[k][1]; i++) {
+		int nxt = arr[k][i+1];
+		if (nxt < 0) return;
 		if (visited[nxt] == 1) continue;
 		dfs(arr, dp, nxt);
 		dp[k][i] += value[k] * value[nxt];
+		dp[k][i] += dp[nxt][arr[nxt][1] - 1];
 		int sum = max(dp[nxt]);
-		for (int j = 0; j <= arr[k][1]; j++) {
-			if (i == j) continue;
+		for (int j = 1; j < arr[k][1]; j++) {
+			if (sum == 0) break;
+			if (i == j)	continue;
 			dp[k][j] += sum;
 		}
 	}
@@ -51,15 +55,15 @@ int main() {
 	for (int i = 2; i <= n; i++) {
 		scanf("%d", &num);
 		arr[num][arr[num][1]++] = i;
-		if (arr[num][1] >= arr[num][0])
+		if (arr[num][1] >= arr[num][0] - 1)
 			fullvector(num, arr);
 	}
 	for (int i = 1; i <= n; i++) {
 		scanf("%d", &value[i]);
 	}
-	int** dp = (int**)malloc(sizeof(int*) * (n + 1));
+	int** dp = (int**)calloc((n + 1), sizeof(int*));
 	for (int i = 1; i <= n; i++) {
-		dp[i] = (int*)malloc(sizeof(int) * arr[i][0]);
+		dp[i] = (int*)calloc(arr[i][1], sizeof(int));
 	}
 
 	dfs(arr, dp, 1);
