ea73c73e764991df12b09a151ab1a3419234ce5f
woong <kswoong1819@gamil.com>
2021-02-28 21:22:15 +0900

    0228

--- Project12/Project12/BOJ_17831.cpp
+++ Project12/Project12/BOJ_17831.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <vector>
+using namespace std;
 
 #define MaxSz 200010
 
@@ -7,14 +9,8 @@ int n;
 int value[MaxSz];
 int visited[MaxSz];
 
-void fullvector(int num, int** arr) {
-	arr[num][0] += 100;
-	realloc(arr[num], sizeof(int*) * arr[num][0]);
-	return;
-}
-
-int max(int* dp) {
-	int x = 1, maxVal = 0;
+int max(vector<int> &dp) {
+	int x = 0, maxVal = 0;
 	while (dp[x] > 0) {
 		if (maxVal < dp[x])
 			maxVal = dp[x];
@@ -23,17 +19,17 @@ int max(int* dp) {
 	return maxVal;
 }
 
-void dfs(int** arr, int** dp, int k) {
+void dfs(vector<vector<int>> &arr, vector<vector<int>> &dp, int k) {
 	visited[k] = 1;
-	for (int i = 1; i < arr[k][1]; i++) {
-		int nxt = arr[k][i+1];
-		if (nxt < 0) return;
+	for (int i = 0; i < arr[k].size(); i++) {
+		int nxt = arr[k][i];
+		if (nxt > n) return;
 		if (visited[nxt] == 1) continue;
 		dfs(arr, dp, nxt);
 		dp[k][i] += value[k] * value[nxt];
-		dp[k][i] += dp[nxt][arr[nxt][1] - 1];
+		dp[k][i] += dp[nxt][arr[nxt].size()];
 		int sum = max(dp[nxt]);
-		for (int j = 1; j < arr[k][1]; j++) {
+		for (int j = 0; j <= arr[k].size(); j++) {
 			if (sum == 0) break;
 			if (i == j)	continue;
 			dp[k][j] += sum;
@@ -46,34 +42,17 @@ int main() {
 	int num;
 	freopen("input.txt", "r", stdin);
 	scanf("%d", &n);
-	int** arr = (int**)malloc(sizeof(int*) * (n + 1));
-	for (int i = 1; i <= n; i++) {
-		arr[i] = (int*)malloc(sizeof(int) * 100);
-		arr[i][0] = 100;
-		arr[i][1] = 2;
-	}
+	vector<vector<int>> arr(n+1);
+	vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
 	for (int i = 2; i <= n; i++) {
 		scanf("%d", &num);
-		arr[num][arr[num][1]++] = i;
-		if (arr[num][1] >= arr[num][0] - 1)
-			fullvector(num, arr);
+		arr[num].push_back(i);
 	}
 	for (int i = 1; i <= n; i++) {
 		scanf("%d", &value[i]);
 	}
-	int** dp = (int**)calloc((n + 1), sizeof(int*));
-	for (int i = 1; i <= n; i++) {
-		dp[i] = (int*)calloc(arr[i][1], sizeof(int));
-	}
 
 	dfs(arr, dp, 1);
 	printf("%d\n", max(dp[1]));
-
-	for (int i = 1; i <= n; i++) {
-		free(arr[i]);
-		free(dp[i]);
-	}
-	free(arr);
-	free(dp);
 	return 0;
 }
