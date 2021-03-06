e50bc42cc2bd9d9123665bdca6a03bd1e84c87c9
kswoong1819 <kswoong1819@gmail.com>
2021-03-06 23:00:00 +0900

    0308

--- Project13/Project13/BOJ_1854.c
+++ Project13/Project13/BOJ_1854.c
@@ -2,14 +2,36 @@
 #include <malloc.h>
 
 #define Ms 1001
-#define INF 10e10
+#define INF 999999999
 #define min(a,b) (a>b ? b : a)
+#define swap(a,b) {int t=a; a=b; b=t;}
 
 int arr[Ms][Ms] = { 0, };
 int dp[Ms][101];
 
-void bfs(int st) {
-
+void bfs(int n, int k, int st) {
+	int queue[Ms * 4];
+	int front = 0, rear = 0, flag = 0;
+	queue[rear++] = st;
+	while (front < rear) {
+		int q = queue[front++];
+		for (int i = 1; i <= n; i++) {
+			if (arr[q][i] > 0) {
+				for (int j = 1; j <= k; j++) {
+					int tmp = dp[q][j-1] + arr[q][i];
+					if (dp[i][j] > tmp && dp[i][j-1] != tmp) {
+						dp[i][j] = tmp;
+						flag = 1;
+					}
+				}
+				if (flag) {
+					queue[rear++] = i;
+					flag = 0;
+				}
+			}
+		}
+	}
+	return;
 }
 
 int main() {
@@ -25,7 +47,12 @@ int main() {
 		scanf("%d %d %d", &s, &e, &c);
 		arr[s][e] = c;
 	}
-	bfs(1);
-
+	bfs(n, k, 1);
+	for (int i = 1; i <= n; i++) {
+		if (dp[i][k] == INF)
+			printf("%d\n", -1);
+		else
+			printf("%d\n", dp[i][k]);
+	}
 	return 0;
 }
