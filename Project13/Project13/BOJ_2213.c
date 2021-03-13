64e9ffc4ef662a2ab468d18ae05c40d24dee3622
kswoong1819 <kswoong1819@gmail.com>
2021-03-13 11:00:00 +0900

    0302_1

--- Project13/Project13/BOJ_2213.c
+++ Project13/Project13/BOJ_2213.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 
-#define MaxSz 1000001
+#define MaxSz 10002
 #define max(a,b) (a>b ? a : b)
 
 typedef struct nd {
@@ -12,8 +12,9 @@ typedef struct nd {
 node* arr[MaxSz];
 int dp[MaxSz][2];
 int visited[MaxSz] = { 0, };
+int check[MaxSz] = { 0, };
 
-int dfs(int k) {
+void dfs(int k) {
 	node* tmp = arr[k];
 	visited[k] = 1;
 	while (tmp) {
@@ -28,13 +29,33 @@ int dfs(int k) {
 	}
 }
 
+void trace(int k, int f) {
+	node* tmp = arr[k];
+	visited[k] = 0;
+	if (f && dp[k][0] > dp[k][1]) {
+		check[k] = 1;
+		f = 0;
+	}
+	else {
+		f = 1;
+	}
+	while (tmp) {
+		if (!visited[tmp->e]) {
+			tmp = tmp->nxt;
+			continue;
+		}
+		trace(tmp->e, f);
+		tmp = tmp->nxt;
+	}
+}
+
 int main() {
 	int n, v, u;
 	node* tmp;
 	freopen("input.txt", "r", stdin);
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++) {
-		dp[i][0] = 1;
+		scanf("%d", &dp[i][0]);
 	}
 	for (int i = 1; i < n; i++) {
 		scanf("%d %d", &v, &u);
@@ -49,6 +70,16 @@ int main() {
 		arr[v] = tmp;
 	}
 	dfs(1);
-	printf("%d\n", n - max(dp[1][0], dp[1][1]));
+	printf("%d\n", max(dp[1][0], dp[1][1]));
+	if (max(dp[1][0], dp[1][1]) == dp[1][0]) {
+		trace(1, 1);
+	}
+	else {
+		trace(1, 0);
+	}
+	for (int i = 1; i <= n; i++) {
+		if(check[i])
+			printf("%d ", i);
+	}
 	return 0;
 }
