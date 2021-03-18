ff07a8be24a71cae8f11fdf379754922b56e2c38
kswoong1819 <kswoong1819@gmail.com>
2021-03-18 23:00:00 +0900

    0318

---
+++ Project13/Project13/BOJ_1647.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int arr[100001];
+
+int main() {
+	int n, m, a, b, c;
+	freopen("input.txt", "r", stdin);
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++) {
+		arr
+	}
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+
+	}
+}
