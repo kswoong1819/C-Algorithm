1464f5114756320501ab6bc98644980667e5efcc
kswoong1819 <kswoong1819@gmail.com>
2021-04-22 23:16:31 +0900

    0422

---
+++ Project14/Project14/BOJ_1761.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct nd {
+	int 
+};
+
+int main() {
+	int n, m, a, b, c;
+	freopen("input.txt", "r", stdin);
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+
+	}
+	return 0;
+}
